c807c654ad77c545e6cb91b1366ec03eca6f28ff
Ilanit Jamilis <ilanitsj@gmail.com>
2017-09-27 23:44:58 -0300

    subo

--- traerDenunciasRadar.php
+++ traerDenunciasRadar.php
@@ -10,22 +10,66 @@ $misParametros = json_decode($misDatos,true);
 $latRecibida = $misParametros["miLat"];
 $lngRecibida = $misParametros["miLng"];
 
-$R = 6371;
+/*$R = 6371;
 $rad = 2;
 
 $maxLat = $latRecibida + rad2deg($rad/$R);
 $minLat = $latRecibida - rad2deg($rad/$R);
 $maxLon = $lngRecibida + rad2deg(asin($rad/$R) / cos(deg2rad($latRecibida)));
-$minLon = $lngRecibida - rad2deg(asin($rad/$R) / cos(deg2rad($latRecibida)));
+$minLon = $lngRecibida - rad2deg(asin($rad/$R) / cos(deg2rad($latRecibida)));*/
+
+function getBoundaries($lat, $lng, $distance = 1, $earthRadius = 6371)
+{
+    $return = array();
+     
+    // Los angulos para cada dirección
+    $cardinalCoords = array('north' => '0',
+                            'south' => '180',
+                            'east' => '90',
+                            'west' => '270');
+    $rLat = deg2rad($lat);
+    $rLng = deg2rad($lng);
+    $rAngDist = $distance/$earthRadius;
+    foreach ($cardinalCoords as $name => $angle)
+    {
+        $rAngle = deg2rad($angle);
+        $rLatB = asin(sin($rLat) * cos($rAngDist) + cos($rLat) * sin($rAngDist) * cos($rAngle));
+        $rLonB = $rLng + atan2(sin($rAngle) * sin($rAngDist) * cos($rLat), cos($rAngDist) - sin($rLat) * sin($rLatB));
+         $return[$name] = array('lat' => (float) rad2deg($rLatB), 
+                                'lng' => (float) rad2deg($rLonB));
+    }
+    return array('min_lat'  => $return['south']['lat'],
+                 'max_lat' => $return['north']['lat'],
+                 'min_lng' => $return['west']['lng'],
+                 'max_lng' => $return['east']['lng']);
+}
+
+$distance = 1; // Sitios que se encuentren en un radio de 1KM
+$box = getBoundaries($latRecibida, $lngRecibida, $distance);
 
 try {
 	$DBH = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 	$DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+	$query = "SELECT latitud, longitud, tipo, descripcion, fecha, 
+	(6371 * ACOS( 
+                                            SIN(RADIANS(latitud)) 
+                                            * SIN(RADIANS(' . $latRecibida . ')) 
+                                            + COS(RADIANS(longitud - ' . $lngRecibida . ')) 
+                                            * COS(RADIANS(latitud)) 
+                                            * COS(RADIANS(' . $latRecibida . '))
+                                            )
+                               ) AS distance
+							   
+        FROM misdenuncias 
+		WHERE (latitud BETWEEN ' . $box['min_lat']. ' AND ' . $box['max_lat'] . ')
+                     AND (longitud BETWEEN ' . $box['min_lng']. ' AND ' . $box['max_lng']. ')
+                     HAVING distance  < ' . $distance . ' ";
 
-	$query = "SELECT latitud, longitud, tipo, descripcion, fecha
+	/*$query = "SELECT latitud, longitud, tipo, descripcion, fecha
         FROM misdenuncias 
         WHERE latitud Between :minLat And :maxLat
-          And longitud Between :minLon And :maxLon";
+          And longitud Between :minLon And :maxLon";*/
 	
 	/*$query = "SELECT latitud, longitud, tipo, descripcion, fecha, 
 	(
@@ -52,6 +96,13 @@ try {
 	/*$params = array(
 	":lat" => $latRecibida,
 	":lng" =>  $lngRecibida
+	);*/
+	
+	/*$params = array(
+	":minLat" => $minLat,
+	":maxLat" =>  $maxLat, 
+	":minLon" => $minLon,
+	":maxLon" =>  $maxLon
 	);*/
 	
 	$params = array(
