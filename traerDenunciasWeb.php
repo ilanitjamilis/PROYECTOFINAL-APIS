3a3a5b8e884d574497d9c9a8848a799c84b2a686
Ilanit Jamilis <ilanitsj@gmail.com>
2017-09-28 18:55:44 -0300

    subo api web

---
+++ traerDenunciasWeb.php
@@ -0,0 +1,56 @@
+<?php
+$servername = "127.0.0.1:51527";
+$username = "azure";
+$password = "6#vWHD_$";
+$dbname = "db";
+
+function parseToXML($htmlStr)
+{
+$xmlStr=str_replace('<','&lt;',$htmlStr);
+$xmlStr=str_replace('>','&gt;',$xmlStr);
+$xmlStr=str_replace('"','&quot;',$xmlStr);
+$xmlStr=str_replace("'",'&#39;',$xmlStr);
+$xmlStr=str_replace("&",'&amp;',$xmlStr);
+return $xmlStr;
+}
+
+// Opens a connection to a MySQL server
+$connection=mysql_connect ($servername, $username, $password);
+if (!$connection) {
+  die('Not connected : ' . mysql_error());
+}
+
+// Set the active MySQL database
+$db_selected = mysql_select_db($dbname, $connection);
+if (!$db_selected) {
+  die ('Can\'t use db : ' . mysql_error());
+}
+
+// Select all the rows in the markers table
+$query = "SELECT latitud, longitud, tipo, descripcion, fecha FROM misdenuncias";
+$result = mysql_query($query);
+if (!$result) {
+  die('Invalid query: ' . mysql_error());
+}
+
+header("Content-type: text/xml");
+
+// Start XML file, echo parent node
+echo '<markers>';
+
+// Iterate through the rows, printing XML nodes for each
+while ($row = @mysql_fetch_assoc($result)){
+  // Add to XML document node
+  echo '<marker ';
+  echo 'latitud="' . $row['latitud'] . '" ';
+  echo 'longitud="' . $row['longitud'] . '" ';
+  echo 'tipo="' . $row['tipo'] . '" ';
+  echo 'descripcion="' . $row['descripcion'] . '" ';
+  echo 'fecha="' . $row['fecha'] . '" ';
+  echo '/>';
+}
+
+// End XML file
+echo '</markers>';
+
+?>
